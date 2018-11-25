f89e4825d355057b5d606891c88c22ec6bb1e040
liuxuan320 <jf940927320@163.com>
2018-11-25 19:30:14 +0800

    增加硬编码embedding.

---
+++ self_layer/embedding/hard_encoding.py
@@ -0,0 +1,13 @@
+__author__ = 'jf'
+from keras.layers import Embedding
+def hard_encoding_layer(tokenizer,embedding_dim):
+    '''
+    硬编码层，可以应用于词性、结构等硬编码
+    :param tokenizer:
+    :param embedding_dim:
+    :return:
+    '''
+    index = tokenizer.word_index
+    num= len(index)+1
+    embedding_layer = Embedding(num, embedding_dim)
+    return embedding_layer
