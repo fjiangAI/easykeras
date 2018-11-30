91227128d9ec89944f5f633de743ece591a624c7
liuxuan320 <jf940927320@163.com>
2018-11-30 10:37:57 +0800

    修改了统计分析的详情，如果有一类别为0，那么可以省略这个类别继续比较。

--- evaluation/classify.py
+++ evaluation/classify.py
@@ -31,13 +31,14 @@ def cal_confusion_matrix(y_true,y_pred,labels=None):
     签
     '''
     return confusion_matrix(y_true, y_pred,labels)
-def report_detail(y_true,y_pred,target_names,digits=4):
+def report_detail(y_true,y_pred,labels,target_names,digits=4):
     '''
     给出各分类的结果及整体平均值，其中整体平均值使用weight的计算方法
     :param y_true:真实标签
     :param y_pred:预测标签
+    :param labels:所有要统计的类别标签
     :param target_names: 类别
     :param digits: 精度，默认小数点后4位
     :return: 统计文本（字符串）
     '''
-    return classification_report(y_true, y_pred,target_names=target_names,digits=digits)
+    return classification_report(y_true, y_pred,labels=labels,target_names=target_names,digits=digits)
