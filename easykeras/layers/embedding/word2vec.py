d7f4a47da43552cef3a2749308e057b746e8b0c8
liuxuan320 <jf940927320@163.com>
2018-12-07 17:05:12 +0800

    Update word2vec.py
    更新了Word2vec的层的参数，使得层可训练且兼容预处理。

--- easykeras/layers/embedding/word2vec.py
+++ easykeras/layers/embedding/word2vec.py
@@ -2,23 +2,20 @@ __author__ = 'jf'
 from gensim.models import KeyedVectors
 from keras.layers import Embedding
 import numpy as np
-def w2v_encoding_layer(tokenizer,embeddings_dic,embedding_dim):
+def w2v_encoding_layer(vocab,embeddings_dic,embedding_dim,trainable=False):
     '''
     使用已有的word2vec模型来获得embedding层
-    :param tokenizer: 所有词的tokenizer
+    :param vocab: 样本词表
     :param embeddings_dic: word2vec模型加载后词典
     :param embedding_dim: 词向量的维度
     :return: embedding层
     '''
-    tokenizer = tokenizer
-    #word_index形式为"词：序号";
-    word_index = tokenizer.word_index
     #样本中词的数目+1
-    num_words = len(word_index)+1
+    num_words = len(vocab)+1
     #创建映射矩阵，行为词的个数，列为词向量长度。
     embedding_matrix = np.zeros((num_words, embedding_dim))
     #对于样本中的每一个词及编号
-    for word, i in word_index.items():
+    for word, i in vocab.items():
         #如果有词向量
         if word in embeddings_dic:
             embedding_matrix[i] = embeddings_dic[word]
@@ -26,5 +23,5 @@ def w2v_encoding_layer(tokenizer,embeddings_dic,embedding_dim):
     embedding_layer = Embedding(num_words,
                                 embedding_dim,
                                 weights=[embedding_matrix],
-                                trainable=False)
+                                trainable=trainable)
     return embedding_layer
