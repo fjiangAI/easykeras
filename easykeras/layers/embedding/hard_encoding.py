6c84eb3c75f723ae19ce462aa32a4776cccdd236
liuxuan320 <jf940927320@163.com>
2018-12-08 11:13:53 +0800

    Update hard_encoding.py
    修改了hard_encoding的参数，符合预处理参数。

--- easykeras/layers/embedding/hard_encoding.py
+++ easykeras/layers/embedding/hard_encoding.py
@@ -1,13 +1,13 @@
 __author__ = 'jf'
 from keras.layers import Embedding
-def hard_encoding_layer(tokenizer,embedding_dim,trainable):
+def hard_encoding_layer(vocab,embedding_dim,trainable):
     '''
     硬编码层，可以应用于词性、结构等硬编码
-    :param tokenizer:
+    :param vocab:
     :param embedding_dim:
     :return:
     '''
-    index = tokenizer.word_index
+    index = len(vocab)
     num= len(index)+1
     embedding_layer = Embedding(num, embedding_dim,trainable=trainable)
     return embedding_layer
