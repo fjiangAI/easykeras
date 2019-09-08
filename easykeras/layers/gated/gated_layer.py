c04a7aad5793d4ff11a94b9597bdce23bfbc269f
liuxuan320 <jf940927320@163.com>
2019-09-08 15:17:20 +0800

    Update gated_layer.py
    更新线性门控单元的相关说明。

--- easykeras/layers/gated/gated_layer.py
+++ easykeras/layers/gated/gated_layer.py
@@ -2,10 +2,10 @@ __author__ = 'jf'
 from keras.layers import Dense,multiply,Conv1D
 from keras.layers import Input
 from keras.models import Model
-def GatedLinearUnit(input_shape=None,kernel_layer="CNN",h_dim=None,c_filters=None,c_kernel_size=None, **kwargs):
+def gated_linear_unit(input_shape=None,kernel_layer="CNN",h_dim=None,c_filters=None,c_kernel_size=None, **kwargs):
     '''
     门控线性单元
-    :param input_shape: 输入维度
+    :param input_shape: 输入的维度，使用输入对象.shape即可
     :param kernel_layer: 使用的核心类型，可以使CNN也可以Dense
     :param h_dim: 使用Dense的隐藏层（输出维度）
     :param c_filters: 使用CNN的核心数（输出维度）
