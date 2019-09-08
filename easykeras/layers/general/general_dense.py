1971fac253cb67174c7649b49a0bf72c9f43751d
liuxuan320 <jf940927320@163.com>
2019-09-08 15:17:40 +0800

    Update general_dense.py
    增加最终线性分类层

--- easykeras/layers/general/general_dense.py
+++ easykeras/layers/general/general_dense.py
@@ -1,9 +1,10 @@
 __author__ = 'jf'
-from keras.layers import Dense,Dropout
-def double_dense(input,hidden_size,hidden_activation="relu",output_size=3,output_activation="softmax",loss=0.6):
+from keras.layers import Dense,Dropout,Input
+from keras.models import Model
+def double_dense_layer(input_shape=None,hidden_size=100,hidden_activation="relu",output_size=3,output_activation="softmax",loss=0.6):
     '''
     双层dense，一般可用于最终的分类
-    :param input: 输入向量
+    :param input_shape: 输入向量的维度
     :param hidden_size: 隐藏dense单元数
     :param hidden_activation: 隐藏层激活函数
     :param output_size: 输出dense单元数
@@ -11,10 +12,12 @@ def double_dense(input,hidden_size,hidden_activation="relu",output_size=3,output
     :param loss:  遗忘概率
     :return: 预测结果
     '''
+    text=Input(shape=(int(input_shape[1]),),)
     ##全连接
-    dense = Dense(hidden_size, activation=hidden_activation)(input)
+    dense = Dense(hidden_size, activation=hidden_activation)(text)
     ##随机损失
     output = Dropout(loss)(dense)
-    #最终预测结果，3分类
+    #最终预测结果
     preds = Dense(output_size, activation=output_activation)(output)
-    return preds
+    model=Model(inputs=[text], outputs=preds)
+    return model
