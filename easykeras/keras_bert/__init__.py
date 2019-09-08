80d87125527970bc5ee0b9c7e59a071c6b6eaaf9
liuxuan320 <jf940927320@163.com>
2019-09-08 15:16:18 +0800

    创建BERT相关文件
    增加了BERT相关文件，使得可以进行微调的BERT和固定词嵌入的使用BERT。

---
+++ easykeras/keras_bert/__init__.py
@@ -0,0 +1,6 @@
+from .bert import *
+from .loader import *
+from .tokenizer import Tokenizer
+from .optimizers import *
+from .util import *
+from .datasets import *
---
+++ easykeras/layers/embedding/bert_embedding.py
@@ -0,0 +1,27 @@
+__author__ = 'jf'
+from easykeras.keras_bert.tokenizer import Tokenizer
+from easykeras.keras_bert import load_vocabulary, get_checkpoint_paths, load_trained_model_from_checkpoint
+from easykeras.keras_bert.util import extract_embeddings,extract_embeddings_bymodel
+from keras.layers import *
+def bert_embedding(model_path,texts_seq):
+    embeddings_seq = extract_embeddings(model_path,texts_seq)
+    return embeddings_seq
+def bert_embedding_bymodel(seq_len,tokenizer,model,texts_seq):
+    embeddings_seq=extract_embeddings_bymodel(seq_len,tokenizer,model,texts_seq)
+    return embeddings_seq
+def getmodelandconfig(modelpath,cased=False):
+    '''
+    获取BERT的配置
+    :param modelpath:
+    :param cased:
+    :return:
+    '''
+    paths = get_checkpoint_paths(modelpath)
+    model = load_trained_model_from_checkpoint(
+            config_file=paths.config,
+            checkpoint_file=paths.checkpoint
+        )
+    vocabs = load_vocabulary(paths.vocab)
+    seq_len = K.int_shape(model.outputs[0])[1]
+    tokenizer = Tokenizer(vocabs, cased=cased)
+    return seq_len,tokenizer,model
