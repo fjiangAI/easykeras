92c8082f2596e58f4891303875217de497ff3cc4
liuxuan320 <jf940927320@163.com>
2019-09-27 22:35:56 +0800

    重写了胶囊网络的方法
    按照新风格编写了胶囊网络的模型，并修改了适配于softmax独热编码的加载训练测试集。

--- easykeras/example/imdb_capsule.py
+++ easykeras/example/imdb_capsule.py
@@ -1,66 +1,69 @@
 __author__ = 'jf'
-from keras.preprocessing import sequence
-from keras.datasets import imdb
-from keras.models import Model
-import sys
-sys.path.append("../../")
+
 from easykeras.layers.capsule import Capsule
 from keras.layers import *
-#epoch 10: 86.29
-# set parameters:
-max_features = 5000
-maxlen = 400
-batch_size = 32
-embedding_dims = 50
-filters = 250
-kernel_size = 3
-hidden_dims = 250
-epochs = 10
-num_classes=2
-from keras import utils
-print('Loading data...')
-(x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_features)
-print(len(x_train), 'train sequences')
-print(len(x_test), 'test sequences')
+from easykeras.example.imdb_util import get_train_test
+import keras
+
+# epoch 10: 86.29
 
 
-#Capsule分类是类似Softmax一样，需要先离散化。
-y_train = utils.to_categorical(y_train, num_classes)
-y_test = utils.to_categorical(y_test, num_classes)
+class CapsuleModel(keras.Model):
+    def __init__(self, config):
+        """
+        初始化模型，准备需要的类
+        :param config: 参数集合
+        """
+        super(CapsuleModel, self).__init__(name='capsule_model')
+        self.embedding_layer = Embedding(config.max_features, config.embedding_dims, input_length=config.max_len)
+        self.dropout_layer = Dropout(0.2)
+        self.conv_layer = Conv1D(config.filters, config.kernel_size, padding='valid', activation='relu', strides=1)
 
-print('Pad sequences (samples x time)')
-x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
-x_test = sequence.pad_sequences(x_test, maxlen=maxlen)
-print('x_train shape:', x_train.shape)
-print('x_test shape:', x_test.shape)
+    def call(self, inputs, mask=None):
+        """
+        构建模型过程,使用函数式模型
+        :return:
+        """
+        embedding_output = self.embedding_layer(inputs)  # (?,400,50)
+        dropout_output = self.dropout_layer(embedding_output)
+        conv_output = self.conv_layer(dropout_output)  # (?,398,250)
+        capsule = Capsule(2, 8, 3, True)(conv_output)  # (?,2,32)
+        output = Lambda(lambda z: K.sqrt(K.sum(K.square(z), 2)))(capsule)  # (?,2)
+        return output
 
-print('Build model...')
-inputs=Input(shape=(400,))
-embedding_layer=Embedding(max_features,
-                    embedding_dims,
-                    input_length=maxlen)
-embedding_output=embedding_layer(inputs) #(?,400,50)
-dropout_layer=Dropout(0.2)
-dropout_output=dropout_layer(embedding_output)
-conv_layer=Conv1D(filters,
-                 kernel_size,
-                 padding='valid',
-                 activation='relu',
-                 strides=1)
-conv_output=conv_layer(dropout_output) #(?,398,250)
-print("conv:")
-print(conv_output.shape)
-capsule = Capsule(2, 8, 3, True)(conv_output) # (?,2,32)
-print("capsule:")
-print(capsule.shape)
-output = Lambda(lambda z: K.sqrt(K.sum(K.square(z), 2)))(capsule) #(?,2)
-print("output:")
-print(output.shape)
-model= Model(inputs=[inputs], outputs=output)
-model.compile(loss='binary_crossentropy',
-              optimizer='adam',
-              metrics=['accuracy'])
-history=model.fit(x_train, y_train,
-          batch_size=batch_size,
-          epochs=epochs,
-          validation_data=(x_test, y_test))
+
+def get_config():
+    return {"max_features": 5000,
+            "max_len": 400,
+            "embedding_dims": 50,
+            "filters": 250,
+            "kernel_size": 3,
+            "hidden_dims": 100,
+            "class_num": 1,
+            "activation": 'sigmoid',
+            "num_classes": 2}
+
+
+class Config:
+    def __init__(self, config={}):
+        for key, value in config.items():
+            setattr(self, key, value)
+
+
+if __name__ == "__main__":
+    # 主程序
+    model_config = Config(get_config())
+    x_train, y_train, x_test, y_test = get_train_test(one_hot=True, num_classes=model_config.num_classes)
+    capsule_model = CapsuleModel(model_config)
+
+    # 设定编译参数
+    loss = 'binary_crossentropy'
+    optimizer = 'adam'
+    metrics = ['accuracy']
+    capsule_model.compile(loss=loss, optimizer=optimizer, metrics=metrics)
+
+    # 设置训练超参数
+    batch_size = 32
+    epochs = 10
+    # 训练过程
+    capsule_model.fit(x=x_train, y=y_train, batch_size=batch_size, epochs=epochs, validation_data=(x_test, y_test))
--- easykeras/example/imdb_util.py
+++ easykeras/example/imdb_util.py
@@ -1,15 +1,21 @@
 from keras.datasets import imdb
 from keras.preprocessing import sequence
+from keras import utils
 
 
-def get_train_test(max_features=5000, max_len=400):
+def get_train_test(max_features=5000, max_len=400, one_hot=False, num_classes=None):
     """
     获取标准IMDB数据集的训练和测试集（与Keras样例一致）
     :param max_features: 默认5000
     :param max_len: 默认400
+    :param one_hot: 标签是否离散化(softmax分类都需要)
+    :param num_classes: 如果标签离散化，则需要指明标签类别数
     :return: 训练样本，训练集标签，测试样本，测试标签
     """
     (x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_features)
     x_train = sequence.pad_sequences(x_train, maxlen=max_len)
     x_test = sequence.pad_sequences(x_test, maxlen=max_len)
+    if one_hot is True:
+        y_train = utils.to_categorical(y_train, num_classes)
+        y_test = utils.to_categorical(y_test, num_classes)
     return x_train, y_train, x_test, y_test
